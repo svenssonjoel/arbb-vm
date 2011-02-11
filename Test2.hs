787ed07674f9ec561574facc75befa2749bb9f23
Ryan Newton <ryan.newton@alum.mit.edu>
2011-02-11 10:30:48 -0500

    Small changes to Test2.hs

--- Test2.hs
+++ Test2.hs
@@ -25,7 +25,9 @@ main = withDefaultContext $ \ctx ->
        binding <- getNullBinding 
        -- This part gets messy! 
        -- TODO: Clean up! 
-       withArray [10.0, 20.0,30.0 :: Float] $ \ input -> 
+--       arr :: ForeignPtr __ <- newArray [10.0, 20.0, 30.0 :: Float]
+
+       withArray [10.0, 20.0, 30.0 :: Float] $ \ input -> 
          do 
           g1 <- createConstant ctx t (castPtr input)
           g2 <- createConstant ctx t (plusPtr (castPtr input) 4) 
@@ -35,10 +37,29 @@ main = withDefaultContext $ \ctx ->
           v3 <- variableFromGlobal ctx g3;
           r  <- createGlobal ctx t "result" binding
           v4 <- variableFromGlobal ctx r 
+
+          v5 <- variableFromGlobal ctx r 
+
+          -- view1 <- variableFromGlobal ctx g1;
+          -- view2 <- variableFromGlobal ctx g1;
+	  -- mutate view2, does view1 change?
+
+          result0 <- readScalarFloat ctx v5
+          putStrLn$ "Value of v5 before function call:"
+          putStrLn (show result0)
+
           execute myfun [v4] [v1,v2,v3]
           -- TODO: Figure out how to best access results (of various types) 
           result <- readScalarFloat ctx v4
+
+          putStrLn$ "First printing result of function call:"
           putStrLn (show result) 
+
+          result2 <- readScalarFloat ctx v5
+          putStrLn$ "Test: reading v5 which was built from same createGlobal"
+          putStrLn (show result2)
+
+          putStrLn "\nNow a ArBB-serialized function:"
           ser <- serializeFunction myfun;
           putStrLn ser
       
