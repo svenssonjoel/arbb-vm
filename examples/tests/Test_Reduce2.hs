0d8a866d8213ac39a190a0160cd24e5c611077e2
Joel Svensson <svensson.bj@gmail.com>
2011-02-22 20:23:29 +0100

    Test_Reduce2.hs does entirely sequential reduce

--- examples/tests/Test_Reduce2.hs
+++ examples/tests/Test_Reduce2.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE CPP, ScopedTypeVariables, CPP #-}
 
+{-  
+    Entirely sequential reduction in ArBB
+-} 
 import Intel.ArbbVM 
 import Intel.ArbbVM.Convenience
 
@@ -21,40 +24,28 @@ main = arbbSession$ do
      one    <- int32_ 1 
      zero   <- int32_ 0 
      ten    <- int32_ 10 
-     tusentjugofyra <- int32_ 1024
-    
+
      print_ "Begin emitting function code.."
 
      add <- funDef_ "add" [sty] [sty,sty] $ \ [out] [a,b] -> do
         op_ ArbbOpAdd [out] [a,b]
-{-        
-     reduce2 <- funDef_ "reduceSpcl" [sty] [dty] $ \ [out] [inp] -> do 
-        len <- createLocal_ size_t "length"     
-        res <- createLocal_ sty "result"
-        in1 <- createLocal_ sty "inputToCall"  
-            
-        op_ ArbbOpLength [len] [inp]
-        op_ ArbbOpCast   [in1] [len] 
-   
-        call_ add [res] [in1,in1]
-        --op_ ArbbOpAdd   [res] [in1,in1]      
-        op_ ArbbOpCopy  [out] [res]  
--}
               
      reduce2 <- funDef_ "reduceSpcl" [sty] [dty] $ \ [out] [inp] -> do 
         len <- createLocal_ size_t "length"     
-        res <- createLocal_ sty "result"
         in1 <- createLocal_ sty "inputToCall" 
         pr  <- createLocal_ sty "partialRes"  
-        lcntr <- createLocal_ sty "loopcounter"  
-        max <- createLocal_ sty "max"   
-            
-        op_ ArbbOpLength [len] [inp]
-        op_ ArbbOpCast   [in1] [len] 
+        lcntr <- createLocal_ sty "loopcounter" 
+       
         
+        -- Obtain length and cast into I32 type
+        op_ ArbbOpLength [len] [inp]
+        op_ ArbbOpCast   [in1] [len]
+   
+        
+        -- Clear loop counter and running sum 
         op_ ArbbOpCopy   [lcntr] [zero]
         op_ ArbbOpCopy   [pr] [zero] 
-        --op_ ArbbOpCopy   [max] [tusentjugofyra]   -- change to 40 for error
+    
          
         while_ 
           ( -- condition
@@ -67,55 +58,40 @@ main = arbbSession$ do
            do 
             tmp <- createLocal_ sty "ls"
             index <- createLocal_ size_t "ix"
+            
             op_ ArbbOpCast [index] [lcntr]
            
-            op_ ArbbOpExtract [tmp] [index,in1]  
-            -- op_ ArbbOpCopy [tmp] [ten]
-            -- call_ add [tmp] [pr,in1]
-            op_ ArbbOpAdd [pr] [pr,tmp]
+            opDynamic_ ArbbOpExtract [tmp] [inp, index]  
+          
+            call_ add [pr] [pr,tmp]
             op_ ArbbOpAdd [lcntr] [lcntr,one]
           )
  
         
         op_ ArbbOpCopy [out] [pr] 
-        --op_ ArbbOpCopy  [out] [max]  
-        --op_ ArbbOpCopy  [out] [lcntr]   
+ 
 
 
 
      liftIO$ putStrLn "Done compiling function, now executing..."
 
-     i_data  <- liftIO$ newArray (replicate 1024 1 :: [Word32])
-     o_data  <- liftIO$ newArray [0 :: Word32]
-     i_array <- createDenseBinding_ (castPtr i_data) 1 [1024] [8]
-     o_array <- createDenseBinding_ (castPtr o_data) 1 [1] [8] 
-     liftIO$ putStrLn "a"  
+     i_data  <- liftIO$ newArray [0..1023 :: Word32] 
+     i_array <- createDenseBinding_ (castPtr i_data) 1 [1024] [4]
+
+  
     
      g_in  <- createGlobal_  dty "in" i_array;       
-     g_out <- createGlobal_  dty "res" o_array;
-     liftIO$ putStrLn "b" 
    
-     v1 <- variableFromGlobal_ g_in;
-     v2 <- variableFromGlobal_ g_out;       
-     liftIO$ putStrLn "c"              
-        
-  --   execute_ reduce [v2] [v1]
-  --   result <- liftIO$ peekArray 1 (castPtr o_data :: Ptr Word32)    
-  --   liftIO$ putStrLn "d"      
-
+     v1 <- variableFromGlobal_ g_in; 
+ 
      binding <- getBindingNull_
      g       <- createGlobal_ sty "res" binding
      y       <- variableFromGlobal_ g
-     liftIO$ putStrLn "e" 
-
+   
      execute_ reduce2 [y] [v1]     
-     liftIO$ putStrLn "f" 
-     
-
+   
      result2 :: Word32 <- readScalar_ y 
-
      
-  --   liftIO$ putStrLn$ "Result: "++ show result
      liftIO$ putStrLn$ "Result2: "++ show result2   
     
      
