7cae6133c92bd57eedab0750f53e699e1f25bea5
Joel Svensson <svensson.bj@gmail.com>
2011-03-15 21:53:36 +0100

    added Test2.hs

---
+++ accelerate/tests/ArBB/Test2.hs
@@ -0,0 +1,46 @@
+
+-- ghci -i.. -Iinclude tests/ArBB/Test2.hs -ltbb -larbb
+
+
+import Data.Array.Unboxed
+import Data.Array.Accelerate
+import Data.Array.Accelerate.Interpreter
+import Data.Array.Accelerate.AST
+
+import qualified Data.Array.Accelerate.Smart as Sugar
+import qualified Data.Array.Accelerate.Array.Sugar as Sugar
+
+
+import           Data.Array.Accelerate.ArBBimm hiding (run)
+import qualified Data.Array.Accelerate.ArBBimm as ArBB 
+
+import Intel.ArbbVM
+import Intel.ArbbVM.Convenience
+
+import Data.Int
+
+import Prelude hiding (map)
+
+incr xs = 
+     let xs' = use xs
+     in map (\x -> x+1) xs'
+
+
+sumUp xs = 
+     let xs' = use xs
+     in fold (+) 0 xs'
+
+
+
+
+
+input = ((fromList  (Sugar.listToShape [1024]) [1..1024 :: Int]) :: Data.Array.Accelerate.Array Sugar.DIM1  Int) 
+
+
+apa = -- arbbSession$ do 
+  let f = incr input
+  in ArBB.run f 
+  
+
+main = do
+     putStrLn$ show apa
