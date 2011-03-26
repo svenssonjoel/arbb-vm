15fbff9d0797e3cb139106e45415efe40e24205e
Joel Svensson <svensson.bj@gmail.com>
2011-03-27 00:24:11 +0100

    Reduction experiments

--- examples/tests/Test_Reduce4.hs
+++ examples/tests/Test_Reduce4.hs
@@ -18,11 +18,122 @@ import Data.Time
     - not limited to pow2 size 
     - should work for 1,2,3D arrays (in the way expected by Accelerate) 
     - In future for for N-Dimensional Arrays.  
+
+   
+   TODO: 
+    - execute_ is asynchronous may enable me to do what I want 
+      (but spread out over many calls into ArBB) 
+
 -}
 
--- loopAccum :: Function -> Variable -> Variable -> EmitArbb () 
--- loopAccum f acc inp = do 
-    
+newGlobalVar t nom = do 
+  bin <- getBindingNull_ 
+  g <- createGlobal_ t nom bin
+  variableFromGlobal_ g     
+
+
+genRed reducer out inp n = do         
+   sty <- getScalarType_ ArbbI32
+   size_t  <- getScalarType_ ArbbUsize
+   dsize_t <- getDenseType_ size_t 1
+   dty <- getDenseType_ sty 1
+   nty <- getNestedType_ sty  
+   
+
+   indices <- newGlobalVar dsize_t "indices" 
+   chunk   <- newGlobalVar size_t "chunk_size" 
+   nested  <- newGlobalVar nty "chunk"         
+   
+   one_chunk <- newGlobalVar dty "achunk" 
+   
+   chunks <- mapM (newGlobalVar dty) ["c" ++ show n | n <- [0..9]]  
+   tmp    <- mapM (newGlobalVar sty) ["t" ++ show n | n <- [0..9]]  
+
+   liftIO$ putStrLn "A"
+   ten <- usize_ 10
+   zero <- usize_ 0
+   one <- usize_ 1
+   two <- usize_ 2
+   three <- usize_ 3
+   four <- usize_ 4
+   five <- usize_ 5
+   six <- usize_ 6
+   seven <- usize_ 7
+   eight <- usize_ 8 
+   nine  <- usize_ 9
+   
+   vs <- usize_ 2  -- This is a Enum VS_OFFSET
+        
+   liftIO$ putStrLn "B"
+   opImm_ ArbbOpDiv [chunk] [n,ten] 
+               
+   liftIO$ putStrLn "C"
+   opDynamicImm_ ArbbOpIndex [indices] [zero,ten,chunk] -- ten chunks! 
+
+   liftIO$ putStrLn "D"
+   -- When it is Dynamic or not, makes no sense to me !!! 
+   opImm_ ArbbOpApplyNesting [nested] [inp,indices,vs]
+               -- NOT SUPPORTED YET BY ARBB !!!! 
+   liftIO$ putStrLn "E"      
+   
+   -- extracts a segment (a dense from a nested)    
+   opImm_ ArbbOpSegment [chunks !! 0] [nested,zero] 
+
+   --liftIO$ putStrLn "F"
+   execute_ reducer [tmp !! 0] [chunks !! 0,chunk]          
+          
+   opImm_ ArbbOpSegment [chunks !! 1] [nested,one] 
+
+   --liftIO$ putStrLn "F1"
+   execute_ reducer [tmp !! 1] [chunks !! 1,chunk]          
+   
+   opImm_ ArbbOpSegment [chunks !! 2] [nested,two] 
+
+   --liftIO$ putStrLn "F2"
+   execute_ reducer [tmp !! 2] [chunks !! 2,chunk]          
+
+   opImm_ ArbbOpSegment [chunks !! 3] [nested,three] 
+
+   --liftIO$ putStrLn "F3"
+   execute_ reducer [tmp !! 3] [chunks !! 3,chunk]          
+
+   opImm_ ArbbOpSegment [chunks !! 4] [nested,four] 
+   --liftIO$ putStrLn "F4"
+   execute_ reducer [tmp !! 4] [chunks !! 4,chunk]          
+
+   opImm_ ArbbOpSegment [chunks !! 5] [nested,five] 
+   --liftIO$ putStrLn "F5"
+   execute_ reducer [tmp !! 5] [chunks !! 5,chunk]          
+
+   opImm_ ArbbOpSegment [chunks !! 6] [nested,six] 
+   --liftIO$ putStrLn "F6"
+   execute_ reducer [tmp !! 6] [chunks !! 6,chunk]          
+   
+   opImm_ ArbbOpSegment [chunks !! 7] [nested,seven] 
+   --liftIO$ putStrLn "F7"
+   execute_ reducer [tmp !! 7] [chunks !! 7,chunk]          
+   
+   opImm_ ArbbOpSegment [chunks !! 8] [nested,eight] 
+   --liftIO$ putStrLn "F8"
+   execute_ reducer [tmp !! 8] [chunks !! 8,chunk]          
+  
+   opImm_ ArbbOpSegment [chunks !! 9] [nested,nine] 
+   opImm_ ArbbOpLength  [chunk] [chunks !! 9]
+   --liftIO$ putStrLn "F9"
+   execute_ reducer [tmp !! 9] [chunks !! 9,chunk]          
+
+
+   opImm_ ArbbOpAdd [tmp !! 0] [tmp !! 0, tmp !! 1]
+   opImm_ ArbbOpAdd [tmp !! 0] [tmp !! 0, tmp !! 2] 
+   opImm_ ArbbOpAdd [tmp !! 0] [tmp !! 0, tmp !! 3]
+   opImm_ ArbbOpAdd [tmp !! 0] [tmp !! 0, tmp !! 4]
+   opImm_ ArbbOpAdd [tmp !! 0] [tmp !! 0, tmp !! 5] 
+   opImm_ ArbbOpAdd [tmp !! 0] [tmp !! 0, tmp !! 6]
+   opImm_ ArbbOpAdd [tmp !! 0] [tmp !! 0, tmp !! 7]
+   opImm_ ArbbOpAdd [tmp !! 0] [tmp !! 0, tmp !! 8] 
+   opImm_ ArbbOpAdd [tmp !! 0] [tmp !! 0, tmp !! 9]
+   
+   opImm_ ArbbOpCopy [out] [tmp !! 0]
 
 main = arbbSession$ do 
      sty <- getScalarType_ ArbbI32
@@ -67,31 +178,6 @@ main = arbbSession$ do
          ) 
         op_ ArbbOpCopy [out] [res]
         
-        
-     genRed <- funDef_ "genRed" [sty] [dty,size_t] $ \ [out] [inp,n] -> do
-        
-        indices <- createLocal_ dsize_t "indices" 
-        chunk   <- createLocal_ size_t "chunk_size" 
-        nested  <- createLocal_ nty "chunk"         
-        results <- createLocal_ nty "partials"
-        
-        ten <- usize_ 10
-        zero <- usize_ 0
-        
-        op_ ArbbOpDiv [chunk] [n,ten] 
-               
-        opDynamic_ ArbbOpIndex [indices] [zero,ten,chunk] -- ten chunks!      
-
-        -- When it is Dynamic or not, makes no sense to me !!! 
-        op_ ArbbOpSplit [nested] [inp,indices]
-        
-        -- ERROR: Map is always at the element level ! 
-        --map_ seqRed [results] [nested] 
-        
-        --"sum" up the results in nested2
-             
-        op_ ArbbOpCast [out] [chunk]             
-
      reduceStep <- funDef_ "rS" [dty] [dty,size_t] $ \ [out] [inp,n] -> do 
         
         parts    <- createLocal_ dty2 "halves"
@@ -162,7 +248,8 @@ main = arbbSession$ do
         t1 <- liftIO getCurrentTime                          
         -- execute_ reduce [y] [vin,n]
         -- execute_ seqRed [y] [vin,n]
-        execute_ genRed [y] [vin,n]
+        --execute_ genRed [y] [vin,n]
+        genRed seqRed y vin n
         finish_
         t2 <- liftIO getCurrentTime 
 
@@ -172,8 +259,8 @@ main = arbbSession$ do
         --liftIO$ putStrLn (getCString str)
         --str <- serializeFunction_ reduce
         --liftIO$ putStrLn (getCString str)
-        str <- serializeFunction_ seqRed
-        liftIO$ putStrLn (getCString str)
+        -- str <- serializeFunction_ seqRed
+        --liftIO$ putStrLn (getCString str)
 
 ---------  
 
