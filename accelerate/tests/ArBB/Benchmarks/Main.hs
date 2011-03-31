8e7b52973d9f106c6c5fb5414f19a475afa120af
Joel Svensson <svensson.bj@gmail.com>
2011-03-31 16:19:15 +0200

    Clean up in Benchmarks

--- accelerate/tests/ArBB/Benchmarks/Main.hs
+++ accelerate/tests/ArBB/Benchmarks/Main.hs
@@ -49,64 +49,62 @@ main = withSystemRandom $ \gen -> do
   t_g_1 <- getCurrentTime
 #if 1
   v1    <- randomUArrayR (-1,1) gen 1000000 
- -- v2    <- randomUArrayR (-1,1) gen 1000000 
+  v2    <- randomUArrayR (-1,1) gen 1000000 
   v1'   <- convertUArray v1
---  v2'   <- convertUArray v2
+  v2'   <- convertUArray v2
 #else
   v1' <- evaluate$ Acc.fromList (Sugar.listToShape [100000]) (replicate 100000 1.0)
-  let v2' = v1' --  <- evaluate$ Acc.fromList (Sugar.listToShape [1000000]) (replicate 1000000 1.0)
+  v2' <- evaluate$ Acc.fromList (Sugar.listToShape [1000000]) (replicate 1000000 1.0)
 #endif
- -- alpha <- uniform gen
+  alpha <- uniform gen
   t_g_2 <- getCurrentTime
   putStrLn$ "Done generating input data: " ++ ( show (diffUTCTime t_g_2 t_g_1) )  
-
-     
-  t_p_1 <- getCurrentTime
-  r' <-  replicateM 100$ evaluate$ ArBB.run (sumAcc v1')
-  t_p_2 <- getCurrentTime
---  r0' <- evaluate$ Interp.run (sumAcc v1') 
---  t_p_3 <- getCurrentTime 
     
-  let r = map Sugar.toList r'
- -- let r0 = Sugar.toList r0'   
+  t_p_1 <- getCurrentTime
+  r' <-  evaluate$ ArBB.run (sumAcc v1')
+  t_p_2 <- getCurrentTime
+  r0' <- evaluate$ Interp.run (sumAcc v1') 
+  t_p_3 <- getCurrentTime 
+    
+  let r =  Sugar.toList r'
+      r0 = Sugar.toList r0'   
 
---  putStrLn$ "Sum: " ++ if checkResult r r0 == [] then "Passed" else ("failed " ++ show (head (checkResult r r0)) ) 
+  putStrLn$ "Sum: " ++ if checkResult r r0 == [] then "Passed" else ("failed " ++ show (head (checkResult r r0)) ) 
   putStrLn$ "Time ArBB : " ++ ( show (diffUTCTime t_p_2 t_p_1) )  
---  putStrLn$ "Time InterP : " ++ ( show (diffUTCTime t_p_3 t_p_2) )  
+  putStrLn$ "Time InterP : " ++ ( show (diffUTCTime t_p_3 t_p_2) )  
   return ()
 
-{- 
+ 
   t_s_1 <- getCurrentTime
   r1' <- evaluate$ ArBB.run (saxpyAcc alpha  v1' v2')
   t_s_2 <- getCurrentTime
---  r2' <- evaluate$ Interp.run (saxpyAcc alpha v1' v2') 
---  t_s_3  <- getCurrentTime
+  r2' <- evaluate$ Interp.run (saxpyAcc alpha v1' v2') 
+  t_s_3  <- getCurrentTime
       -- r3 = Sugar.toList$ CUDA.run (saxpyAcc alpha v1' v2') 
   let r1 = Sugar.toList r1'
-  --    r2 = Sugar.toList r2'   
+      r2 = Sugar.toList r2'   
 
---  putStrLn$ "Saxpy: " ++ if checkResult r1 r2 == [] then "Passed" else "failed" 
+  putStrLn$ "Saxpy: " ++ if checkResult r1 r2 == [] then "Passed" else "failed" 
   putStrLn$ "Time ArBB : " ++ ( show (diffUTCTime t_s_2 t_s_1) )  
- -- putStrLn$ "Time InterP : " ++ ( show (diffUTCTime t_s_3 t_s_2) )  
-  -- putStrLn$ show $ take 10 $ checkResult r1 r2
+  putStrLn$ "Time InterP : " ++ ( show (diffUTCTime t_s_3 t_s_2) )  
 
  
   t_dp_1 <- getCurrentTime                          
   r4' <- evaluate$  ArBB.run (dotpAcc v1' v2') 
   t_dp_2 <- getCurrentTime
---  r5' <- evaluate$  Interp.run (dotpAcc v1' v2')   
-  --t_dp_3 <- getCurrentTime
+  r5' <- evaluate$  Interp.run (dotpAcc v1' v2')   
+  t_dp_3 <- getCurrentTime
 
   let r4 = Sugar.toList r4'
-   --   r5 = Sugar.toList r5'
+      r5 = Sugar.toList r5'
   
-  --putStrLn$ "DotProduct: " ++ if checkResult r4 r5 == [] then "Passed" else ("failed " ++ show (head (checkResult r4 r5)) )
+  putStrLn$ "DotProduct: " ++ if checkResult r4 r5 == [] then "Passed" else ("failed " ++ show (head (checkResult r4 r5)) )
   putStrLn$ "Time ArBB : " ++ ( show (diffUTCTime t_dp_2 t_dp_1) )  
- -- putStrLn$ "Time InterP : " ++ ( show (diffUTCTime t_dp_3 t_dp_2) )  
+  putStrLn$ "Time InterP : " ++ ( show (diffUTCTime t_dp_3 t_dp_2) )  
   
 
   return ()
--}
+
 
 checkResult [] [] = [] 
 checkResult (x:xs) (y:ys) | 0.001 < abs (x - y) = (x,y) : checkResult xs ys 
