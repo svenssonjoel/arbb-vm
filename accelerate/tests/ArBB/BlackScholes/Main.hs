74d90dc63fd79d066d15e7c2039107f8acf3224c
Joel Svensson <svensson.bj@gmail.com>
2011-04-06 15:38:44 +0200

    improved upon benchmarking in BlackScholes

--- accelerate/tests/ArBB/BlackScholes/Main.hs
+++ accelerate/tests/ArBB/BlackScholes/Main.hs
@@ -23,7 +23,7 @@ import qualified Data.Array.Accelerate.ArBB as ArBB
 
 
 -- CUDA back-end 
--- import qualified Data.Array.Accelerate.CUDA as CUDA
+import qualified Data.Array.Accelerate.CUDA as CUDA
 
 import Data.Int
 import Control.Exception
@@ -33,38 +33,70 @@ import Data.Time
 import System.Random.MWC 
 import Random -- accelerate-examples/src/common/Random.hs
 
+import System.IO
+import System.Exit
+import System.Directory
+import System.Environment
 
-n = 2^20
 
-main = withSystemRandom $ \gen -> do
+
+main :: IO ()
+main = do
+  args <- getArgs 
+  case args of 
+     [n] -> run (read n) True 
+     [n,w] -> run (read n) (w == "y") 
+     _ -> error "wrong arg"
+    
+  
+run n w = withSystemRandom $ \gen -> do
   putStrLn "Generating input data..." 
   t_g_1 <- getCurrentTime
   v_sp <- randomUArrayR (3,30)     gen n
   v_os <- randomUArrayR (1,100)    gen n
   v_oy <- randomUArrayR (0.25, 10) gen n 
   t_g_2 <- getCurrentTime
-
   v_psy :: IArray.Array Int (Float,Float,Float) <- evaluate$ listArray (0,n-1) $ zip3 (elems v_sp) (elems v_os) (elems v_oy)
   a_psy <- evaluate$ Acc.fromIArray v_psy
-
-  t_g_3 <- getCurrentTime
-  
-  
-  
+  t_g_3 <- getCurrentTime 
   putStrLn$ "Done generating input data: " ++ ( show (diffUTCTime t_g_2 t_g_1) )  
   putStrLn$ "Zip stage took: " ++ show (diffUTCTime t_g_3 t_g_2) 
 
+  if w 
+   then do 
+    n_sp <- randomUArrayR (3,30)     gen 2048
+    n_os <- randomUArrayR (1,100)    gen 2048
+    n_oy <- randomUArrayR (0.25, 10) gen 2048
+    n_g_2 <- getCurrentTime
+    n_psy :: IArray.Array Int (Float,Float,Float) <- evaluate$ listArray (0,2047) $ zip3 (elems n_sp) (elems n_os) (elems n_oy)
+    na_psy <- evaluate$ Acc.fromIArray n_psy
+
+    r <-  evaluate$ ArBB.run (blackscholesAcc na_psy)
+    putStrLn$ "warmed up ArBB: " ++ show (head (toList r))
+    r <- evaluate$ CUDA.run (blackscholesAcc na_psy) 
+    putStrLn$ "warmed up CUDA: " ++ show (head (toList r)) 
+   else return () 
+
+  
+
+  -- The timing run 
   t_p_1 <- getCurrentTime
   r' <-  evaluate$ ArBB.run (blackscholesAcc a_psy)
   t_p_2 <- getCurrentTime
---   r0' <- evaluate$ CUDA.run (blackscholesAcc a_psy) 
+  r0' <- evaluate$ CUDA.run (blackscholesAcc a_psy) 
   t_p_3 <- getCurrentTime 
 
---  putStrLn$ "BlackScholes: " ++ if checkResult r r0 == [] then "Passed" else "failed "
+  putStrLn$ "BlackScholes: " ++ if checkResult r r0 then "Passed" else "failed "
   putStrLn$ "Time ArBB : " ++ ( show (diffUTCTime t_p_2 t_p_1) )  
   putStrLn$ "Time CUDA : " ++ ( show (diffUTCTime t_p_3 t_p_2) )  
 
---   putStrLn$ show$ take 1$ Prelude.zip (toList r') (toList r0')
---   putStrLn$ show$ toList r0'
-  
   return ()
+
+
+
+checkResult [] [] = True
+checkResult ((a,a'):as) ((b,b'):bs) = abs (a - b) < 0.001 &&
+                                      abs (a' - b') < 0.001 && 
+                                      checkResult as bs 
+checkResult _ _ = False
+
