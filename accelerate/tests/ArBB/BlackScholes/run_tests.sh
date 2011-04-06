1a4e8efd238281ac3aaaeb86c669a1bf9089d705
Joel Svensson <svensson.bj@gmail.com>
2011-04-06 16:12:04 +0200

    new benchmark script

--- accelerate/tests/ArBB/BlackScholes/run_tests.sh
+++ accelerate/tests/ArBB/BlackScholes/run_tests.sh
@@ -1,28 +1,17 @@
 #!/bin/sh
 
-echo **** RUNNING TEST CUDA BACK-END WARMED UP ****
-
-./Main 100000 y 
-./Main 250000 y 
-./Main 500000 y 
-./Main 750000 y
-./Main 1000000 y
-./Main 2500000 y
-./Main 5000000 y
-./Main 7500000 y 
-./Main 10000000 y
-./Main 20000000 y 
-
-echo **** RUNNING TEST WITHOUT WARMING UP CUDA ****
-
-./Main 100000 n 
-./Main 250000 n 
-./Main 500000 n 
-./Main 750000 n
-./Main 1000000 n
-./Main 2500000 n
-./Main 5000000 n
-./Main 7500000 n 
-./Main 10000000 n
-./Main 20000000 n 
-
+echo RUNNING TEST WITH CUDA BACK-END WARMED UP
+echo ------------------------------------------------------------------------------
+for i in 100000 250000 500000 750000 1000000 2500000 5000000 7500000 10000000 20000000; do 
+   echo Running test with $i elements
+   ./Main $i y 
+done
+echo ------------------------------------------------------------------------------
+echo 
+echo 
+echo RUNNING TEST WITH CUDA BACK-END ***NOT*** WARMED UP
+echo ------------------------------------------------------------------------------
+for i in 100000 250000 500000 750000 1000000 2500000 5000000 7500000 10000000 20000000; do 
+   echo Running test with $i elements
+   ./Main $i n 
+done
