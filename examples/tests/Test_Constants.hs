388e177041416159b477e9afa27dbed30a04580e
Joel Svensson <svensson.bj@gmail.com>
2011-10-02 11:03:53 +0200

    updated Test_Constants.hs

--- examples/tests/Test_Constants.hs
+++ examples/tests/Test_Constants.hs
@@ -1,62 +1,31 @@
-
+{-# LANGUAGE ScopedTypeVariables #-}
 
 import Intel.ArbbVM 
+import Intel.ArbbVM.Convenience
 
-import Foreign.Marshal.Array
-import Foreign.Ptr 
+{- BJS: updated 2 oct 2011
+        Now uses Convenienve library 
+-} 
 
-import C2HS
-
-readScalarOfSize n ctx v = 
-    allocaBytes n $ \ptr -> 
-       do       
-        readScalar ctx v ptr 
-        peek (castPtr ptr)
-
-newConstantAlt :: Storable a => Context -> ScalarType -> a -> IO Variable 
-newConstantAlt ctx st n = 
-  do           
-   t   <- getScalarType ctx st       
-   tmp <- withArray [n] $ \x -> createConstant ctx t (castPtr x)
-   variableFromGlobal ctx tmp
-  
-
-main = do 
-     ctx <- getDefaultContext 
-     t   <- getScalarType ctx ArbbF32
-     fnt <- getFunctionType ctx [t] [t,t] 
-     myfun <- beginFunction ctx fnt "add" 0
-     a     <- getParameter myfun 0 0 
-     b     <- getParameter myfun 0 1
-     c     <- getParameter myfun 1 0 
+----------------------------------------------------------------------------
+-- constants declared inside a function and outside.
+main = arbbSession$ do 
+   
+     t   <- getScalarType_ ArbbF32
+   
+     myfun <- funDef_ "add" [t] [t,t] $ \[out] [in1,in2] -> do 
+       tmp <- float32_ 1.0 -- constant
      
-     -- tmp <- newConstant ctx t (1.0 :: Float) 
-     tmp <- newConstantAlt ctx ArbbF32 (1.0 :: Float)
-     -- tmp2 <- newConstantAlt ctx ArbbF32 (1.0 :: Float)
-     
-     -- These two will differ! 
-     --putStrLn $ show $ castPtr $ fromVariable tmp
-     --putStrLn $ show $ castPtr $ fromVariable tmp2 
-     
-     op myfun ArbbOpAdd [c] [a,tmp]
-     op myfun ArbbOpMul [c] [c,b]
-     endFunction myfun
-     --compile myfun
-     -- binding <- getBindingNull 
-     -- This part gets messy! 
-     -- TODO: Clean up! 
-     withArray [10.0, 2.0 :: Float] $ \ input -> 
-        do 
-          g1 <- createConstant ctx t (castPtr input)
-          g2 <- createConstant ctx t (plusPtr (castPtr input) 4) 
+       op_ ArbbOpAdd [out] [in1,tmp]
+       op_ ArbbOpMul [out] [out,in2]
+   
+     v1 <- float32_ 1.0 --constant
+     v2 <- float32_ 2.0 --constant
          
-          v1 <- variableFromGlobal ctx g1;
-          v2 <- variableFromGlobal ctx g2;
-         
-          r  <- createGlobalNB ctx t "result" --binding
-          v3 <- variableFromGlobal ctx r 
-          execute myfun [v3] [v1,v2]
-          -- TODO: Figure out how to best access results (of various types) 
-          result <- readScalarOfSize 4 ctx v3 :: IO Float
-          putStrLn (show result) 
+     r  <- createGlobal_nobind_ t "result" 
+     v3 <- variableFromGlobal_ r 
+     execute_ myfun [v3] [v1,v2]
+        
+     result :: Float <- readScalar_ v3 
+     liftIO$ putStrLn (show result) 
       
